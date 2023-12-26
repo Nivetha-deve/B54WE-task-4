056e68f655a0f3eceb6d10f48564114b218551bc
Nivetha-deve <142766302+Nivetha-deve@users.noreply.github.com>
2023-12-26 11:52:39 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,13 @@
+console.log( "RESUME");
+const jsondata = {
+    obj:{
+    Name : "NIVETHA SHREE",
+    NUMBER: 6380467388,
+    EMAIL:"nivethashree028@gmail.com",
+    QUALIFICATION:"BSC.MATHEMATICS in Sri Krishna arts and science college,coimbatore in 2021 with 73% " ,
+    Interested: "Javascript,Microsoft,SQL",
+    PersonalTraits: "1)Team work, 2) hard working, 3) adaptable",
+    Details: " FatherName:KUMARASAMY,MotherName:Dhanalakshmi,DOB:15/6/2000,Gender:Female,Nationality:India,Hobby;Playing game,reading books,cooking",
+    Declaration: "I hereby declare that all the above details are true and best of my knowledge."
+}} ;
+console.log(jsondata.obj);
